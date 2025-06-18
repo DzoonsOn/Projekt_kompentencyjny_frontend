60ba37b4a0b7122c60fa5624c044cbb199f955f5
Zuzansas <zuzanna.niciak@gmail.com>
2025-06-18 10:17:24 +0200

    New view

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,76 +1,82 @@
-'use client'
+"use client";
 
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/navigation'
-import '../../components/style/Categories.css'
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import "../../components/style/Categories.css";
 
 type Department = {
-	departmentId: number
-	departmentName: string
-}
+  departmentId: number;
+  departmentName: string;
+};
 
 type Category = {
-	department: Department
-	count: number
-}
+  department: Department;
+  count: number;
+};
 
 export default function Categories() {
-	const [categories, setCategories] = useState<Category[]>([])
-	const [loading, setLoading] = useState(true)
-	const router = useRouter()
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
 
-	useEffect(() => {
-		const fetchCategories = async () => {
-			try {
-				const res = await fetch('https://localhost:5001/productShortage/Categories', {
-					headers: {
-						Accept: 'text/plain',
-					},
-				})
-				if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`)
-				const data: Category[] = await res.json()
-				setCategories(data)
-			} catch (error) {
-				console.error('Błąd pobierania kategorii:', error)
-			} finally {
-				setLoading(false)
-			}
-		}
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const res = await fetch(
+          "https://localhost:5001/productShortage/Categories",
+          {
+            headers: {
+              Accept: "text/plain",
+            },
+          }
+        );
+        if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
+        const data: Category[] = await res.json();
+        setCategories(data);
+      } catch (error) {
+        console.error("Błąd pobierania kategorii:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-		fetchCategories()
-	}, [])
+    fetchCategories();
+  }, []);
 
-	const handleRedirect = (departmentId: number) => {
-		router.push(`/products?departmentId=${departmentId}`)
-	}
+  const handleRedirect = (departmentId: number) => {
+    router.push(`/products?departmentId=${departmentId}`);
+  };
 
-	return (
-		<div style={{ textAlign: 'center', padding: '20px' }}>
-			<h1 className='header'>
-				<div className='redDivStyle'>Kamery</div>
-			</h1>
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1 className="header">
+        <div className="redDivStyle">Kamery</div>
+      </h1>
 
-			<div className='title'>
-				<h2>Kategorie</h2>
-			</div>
+      <div className="title">
+        <h2>Kategorie</h2>
+      </div>
 
-			{loading ? (
-				<div className='loadingScreen'>
-					<p className='text-xl text-red-600 font-bold animate-pulse'>Ładowanie kategorii...</p>
-				</div>
-			) : (
-				<div className='gridContainer'>
-					{categories.map(item => (
-						<button
-							key={item.department.departmentId}
-							className='button'
-							onClick={() => handleRedirect(item.department.departmentId)}>
-							{item.department.departmentName}
-							<div className='redSquareStyle'>{item.count}</div>
-						</button>
-					))}
-				</div>
-			)}
-		</div>
-	)
+      {loading ? (
+        <div className="loadingScreen">
+          <p className="text-xl text-[#99ccff] font-bold animate-pulse">
+            Ładowanie kategorii...
+          </p>
+        </div>
+      ) : (
+        <div className="gridContainer">
+          {categories.map((item) => (
+            <button
+              key={item.department.departmentId}
+              className="button"
+              onClick={() => handleRedirect(item.department.departmentId)}
+            >
+              {item.department.departmentName}
+              <div className="redSquareStyle">{item.count}</div>
+            </button>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,25 @@
-'use client'
+"use client";
 const Footer = () => {
-	return (
-		<div className='flex flex-col md:flex-row items-center justify-center px-4 h-[5rem] border rounded-md bg-red-50 shadow-md mt-auto w-full'>
-			<div className='flex items-center'>
-				<span className='text-red-600 font-semibold'>Aplikacja: ShelfDetect v1.0</span>
-			</div>
-			<div className='flex items-center space-x-6 mt-2 md:mt-0'>
-				<div className='flex items-center text-gray-600'>
-					<i className='fas fa-calendar-alt mr-1'></i>
-					<span className='text-red-500 text-sm'>Kontakt: support@shelfdetect.com</span>
-				</div>
-			</div>
-		</div>
-	)
-}
+  return (
+    <div
+      className="flex flex-col md:flex-row items-center justify-center px-4 h-[5rem] border rounded-md shadow-md mt-auto w-full"
+      style={{ backgroundColor: "#99ccff" }}
+    >
+      <div className="flex items-center">
+        <span className=" font-semibold" style={{ color: "#4169E1" }}>
+          Aplikacja: ShelfDetect v1.0
+        </span>
+      </div>
+      <div className="flex items-center space-x-6 mt-2 md:mt-0">
+        <div className="flex items-center text-gray-600">
+          <i className="fas fa-calendar-alt mr-1"></i>
+          <span className="text-sm" style={{ color: "#4169E1" }}>
+            Kontakt: support@shelfdetect.com
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
